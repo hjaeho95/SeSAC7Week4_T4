f60f8f11265143b6d2c000f2813e6fc39d286221
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-23 15:23:20 +0900

    Feat: View, ViewControllerProtocol 작성

---
+++ SeSAC7Week4_T4/Protocol/SeSACViewControllerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  SeSACViewControllerProtocol.swift
+//  SeSAC7Week4_T4
+//
+//  Created by ez on 7/23/25.
+//
+
+import Foundation
+
+protocol SeSACViewControllerProtocol: SeSACViewProtocol {
+    func configuerHierarchy()
+    func configureLayout()
+    func configureUI()
+}
---
+++ SeSAC7Week4_T4/Protocol/SeSACViewProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  SeSACViewProtocol.swift
+//  SeSAC7Week4_T4
+//
+//  Created by ez on 7/23/25.
+//
+
+import Foundation
+
+@objc protocol SeSACViewProtocol {
+    @objc optional static var identifier: String { get }
+}
